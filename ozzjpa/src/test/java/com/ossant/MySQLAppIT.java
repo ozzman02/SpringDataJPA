c9f11cce89ab00406cdea7a7ca489ad952e070ec
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-06-19 19:22:45 -0600

    initial project setup

---
+++ ozzjpa/src/test/java/com/ossant/MySQLAppIT.java
@@ -0,0 +1,24 @@
+package com.ossant;
+
+import com.ossant.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MySQLAppIT {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testMySQL() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isGreaterThan(0);
+    }
+
+}
